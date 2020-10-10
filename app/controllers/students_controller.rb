271b65acf7d59255479ab65ce51a6b04d9c072d0
Faruk AYDIN <omerfaruk26@gmail.com>
2020-10-10 13:00:46 +0200

    Implement 'POST /students' endpoint to create student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,20 @@ class StudentsController < ApplicationController
   def index
     render json: Student.all
   end
+
+  def create
+    student = Student.new(student_params)
+
+    if student.save
+      return render json: student, status: :ok
+    end
+
+    render json: student.errors, status: :unprocessable_entity
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :phone_number)
+  end
 end
