c988cde3741d55e3402f090e103eb8026b507a7f
Faruk AYDIN <omerfaruk26@gmail.com>
2020-10-10 16:53:03 +0200

    Implement SMS service by using Hubraum API

---
+++ lib/sms.rb
@@ -0,0 +1,19 @@
+class Sms
+  attr_reader :client
+
+  HUBRAUM_SMS_ENDPOINT = ENV['HUBRAUM_SMS_ENDPOINT'].freeze
+  TADHACK_AUTHORIZATION_TOKEN = ENV['TADHACK_AUTHORIZATION_TOKEN'].freeze
+
+  def initialize(client: HTTParty)
+    @client = client
+  end
+
+  def send(message:, receiver:, sender: 'hubraum')
+    options = { 
+      body: "From=#{sender}&To=#{receiver}&Body=#{message}",
+      headers: { "Authorization": TADHACK_AUTHORIZATION_TOKEN }
+    }
+
+    client.post(HUBRAUM_SMS_ENDPOINT, options)
+  end
+end
