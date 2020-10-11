ba22eb10ba281788dc933d9f7a184b6455b52b11
Faruk AYDIN <omerfaruk26@gmail.com>
2020-10-11 10:54:40 +0200

    Send SMS after creating a message

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,18 @@ class Message < ApplicationRecord
   enum message_type: { text: 0, audio: 1 }
   
   validates :content, presence: true
+
+  before_create :send_sms
+
+  private
+
+  def send_sms
+    if(message_type.text == text)
+      Sms.new.send(
+        message: content,
+        sender: sender.phone_number,
+        receiver: receiver.phone_number
+      )
+    end
+  end
 end
